7e3d2fb71b9ddac96dc2c6eed7f7e14d0d281fe9
kenkanyago <kanyagoken@gmail.com>
2024-06-08 08:47:52 +0300

    My tic-tac-toe

---
+++ my-ti-tac-toe/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return ( 
+    <>
+    <div className='board-row'>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+    </div>
+  
+    <div className='board-row'>
+
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+    </div>
+
+    <div className='board-row'>
+
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+      <button className='square'>X</button>
+    </div>
+
+  </>
+);
+  
+}
+
+export default App
